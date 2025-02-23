e94cf83a561bec8809ebd008e0cba1895ec82179
Burçak <burcakgonulaydin@gmail.com>
2025-02-23 14:40:29 +0300

    Initial commit

--- if_statements.cpp
+++ if_statements.cpp
@@ -4,7 +4,30 @@
  using namespace std;
 
 
-int getMax(int num1, int num2){
+int getMax(int num1, int num2, int num3, int num4){
+    int result;
+
+    if(num1 >= num2 && num1 >= num3) {
+
+        result = num1;
+    } else if (num2 >= num1 && num2 >= num3){
+        result = num2;
+    } 
+    else {
+        result = num3;
+    }
+
+    return result;
+
+}
+
+ int main()
+ {
+    cout << getMax(3,5,9,7);
+ }
+
+
+/*  int getMax(int num1, int num2){
     int result;
 
     if(num1 > num2) {
@@ -16,14 +39,7 @@ int getMax(int num1, int num2){
 
     return result;
 
-}
-
- int main()
- {
-
-    cout << getMax(3,5);
- }
-
+} */
 
 
 /*  int main()
